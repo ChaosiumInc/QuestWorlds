6c19c6e2cb08f492aa5ce7e7af028072e0ce452a
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-08-13 01:09:17 +0100

    Finish the you/your/we edit and additions to clarify obstacles and resistance.

--- create_markdown.sh
+++ create_markdown.sh
@@ -1,3 +1,3 @@
-cat 1.Introduction_Participants.md 2.Core_Mechanics.md 3.Character_Creation.md 4.Optional_Mechanics.md 5.Simple_Contests.md 6.Hero_Points.md 7.Relationships.md 8.Extended_Contests.md 9.Alternative_Character_Creation.md 10.Community_Resources_and_Support.md 11.Appendix.md > markdown/QuestWorlds.md
+cat 1.Introduction_Participants.md 2.Core_Mechanics.md 3.Character_Creation.md 4.Optional_Mechanics.md 5.Simple_Contests.md 6.Hero_Points.md 7.Relationships.md 8.Lonsg_Contests.md 9.Alternative_Character_Creation.md 10.Community_Resources_and_Support.md 11.Appendix.md > markdown/QuestWorlds.md
 cat 1.Introduction_Participants.md 2.Core_Mechanics.md 3.Character_Creation.md 5.Simple_Contests.md 6.Hero_Points.md 7.Relationships.md 11.Appendix.md > markdown/QuestWorlds_Quickstart.md 
 
