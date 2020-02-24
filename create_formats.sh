f61a05ff4221b9340089ec44a74e4f30722dab7d
iancooper <ian_hammond_cooper@yahoo.co.uk>
2020-02-24 00:44:21 +0000

    add header and footers to the PDF file

--- create_formats.sh
+++ create_formats.sh
@@ -1,7 +1,7 @@
 pandoc -f markdown -t html markdown/QuestWorlds.md -o docs/QuestWorlds.html 
 pandoc -f markdown -s -t rtf markdown/QuestWorlds.md -o docs/QuestWorlds.rtf 
 # We need to do this as we generate tex that won't convert to PDF without manually accepting errors 
-pandoc -s --toc markdown/QuestWorlds.md -o docs/QuestWorlds.tex
+pandoc -s --toc markdown/QuestWorlds.md -o docs/QuestWorlds.tex -H footers.tex
 pdflatex -output-directory=docs docs/QuestWorlds.tex
 
 
