e26feaf99639d0bd9a5a1f9f569701e6ca4a984a
iancooper <ian_hammond_cooper@yahoo.co.uk>
2020-02-25 01:01:22 +0000

    Trying to fix fonts

--- create_formats.sh
+++ create_formats.sh
@@ -1,8 +1,8 @@
 pandoc -f markdown -t html markdown/QuestWorlds.md -o docs/QuestWorlds.html 
 pandoc -f markdown -s -t rtf markdown/QuestWorlds.md -o docs/QuestWorlds.rtf 
 # We need to do this as we generate tex that won't convert to PDF without manually accepting errors 
-pandoc -s --toc markdown/QuestWorlds.md -o docs/QuestWorlds.tex -H footers.tex
-pdflatex -output-directory=docs docs/QuestWorlds.tex
+pandoc markdown/QuestWorlds.md -s --toc  --pdf-engine=xelatex -H footers.tex -V 'mainfont:CormorantGaramond' -o docs/QuestWorlds.pdf 
+# pdflatex -output-directory=docs docs/QuestWorlds.tex
 
 
 
