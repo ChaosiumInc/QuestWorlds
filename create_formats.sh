b2cecd7652d8859c28de252ad00d7f95a79e6274
iancooper <ian_hammond_cooper@yahoo.co.uk>
2020-03-01 16:28:13 +0000

    Try to get as close as we can to layout example

--- create_formats.sh
+++ create_formats.sh
@@ -1,7 +1,8 @@
 pandoc -f markdown -t html markdown/QuestWorlds.md -o docs/QuestWorlds.html 
 pandoc -f markdown -s -t rtf markdown/QuestWorlds.md -o docs/QuestWorlds.rtf 
-# We need to do this as we generate tex that won't convert to PDF without manually accepting errors 
-pandoc markdown/QuestWorlds.md -s --toc  --pdf-engine=xelatex -H footers.tex -V 'mainfont:CormorantGaramond' -o docs/QuestWorlds.pdf 
+pandoc markdown/QuestWorlds.md -s --toc  --pdf-engine=xelatex --template=chaosium.latex -H footers.tex -V geometry=margin=1.25in -V mainfont:'Garamond' -V fontsize=11pt -o docs/QuestWorlds.pdf
+# To use this for debugging, set QuestWorlds.pdf to QuestWorlds.tex above, which will generate a tex file you can debug, or convert to PDF
+# Note that some options, such as changing the font, won't work with this
 # pdflatex -output-directory=docs docs/QuestWorlds.tex
 
 
